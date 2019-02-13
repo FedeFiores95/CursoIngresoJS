708f20636840fc9508c8d562e0c7018530a2bfd1
alumno <alumno>
2019-02-13 09:19:05 -0300

    ejercicio 2 simulacro de parcial 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,17 @@
 function mostrar()
 {
   
+	var precioEfectivo;
+	var recargoTarjeta;
+	var PrecioTarjeta;
+
+	precioEfectivo = elNombre.value;
+	precioEfectivo = parseInt(precioEfectivo);
+
+	recargoTarjeta = precioEfectivo *10/100;
+
+	PrecioTarjeta = precioEfectivo + recargoTarjeta;
+
+	alert("El precio del producto en efectivo es de $" + precioEfectivo + ", con tarjeta tiene un recargo del 10%, que seria $" + recargoTarjeta + " de recargo, costandole de precio final $" + PrecioTarjeta);
+
 }
