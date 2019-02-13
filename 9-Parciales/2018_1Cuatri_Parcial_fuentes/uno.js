da6effb417db3f3be7de69f18e3acc77df06c749
alumno <alumno>
2019-02-13 09:08:52 -0300

    simulacro de parcial 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,23 @@
 
 function mostrar()
 {
+	var personaje1;
+	var personaje2;
+	var edad1;
+	var edad2;
+	var sumaEdades;
+
+	personaje1 = prompt("Ingrese el nombre del personaje 1");
+	edad1 = prompt("Ingrese la edad del personaje 1");
+	edad1 = parseInt(edad1);
+
+	personaje2 = prompt("Ingrese el nombre del personaje 2");
+	edad2 = prompt("Ingrese la edad del personaje 2");
+	edad2 = parseInt(edad2);
+
+	sumaEdades = edad1 + edad2;
+
+	alert("Ustedes son " + personaje1 + " y " + personaje2 + ", sus edades son " + edad1 + " y " + edad2 + ", y la suma de sus edades es " + sumaEdades);
 
 }
+
