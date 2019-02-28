31098494271d2fd06c6244d4fd7708ecd395754b
fedefiores95 <fedefiores95@gmail.com>
2019-02-28 06:07:51 -0300

    ejercicios de parcial

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,107 @@
 function mostrar()
 {
+	var animal;
+	var pesoAnimal;
+	var tempHabitat;
+	var continuar=true;
+	var cantTempPares = 0;
+	var contador = 0;
+	var animalMasPesado;
+	var tempAnimalMasPesado;
+	var cantAnimalesBajoCero = 0;
+	var sumaPesoAnimales = 0;
+	var	pesoAnimalMasPesadoBajoCero;
+	var pesoAnimalMenosPesadoBajoCero;
+	var cantAnimales = 0;
+	var pesoAnimalMasPesado;
+
+	while (continuar==true)
+	 {
+	 	do
+	 	 {
+	 	 	animal = prompt("Nombre del animal");
+	 	 }while(isNaN(animal) == false);
+
+	 	do
+	 	 {
+	 	 	pesoAnimal = prompt("Peso del animal (Solo un numero entre 1 y 1000)");
+	 	 	pesoAnimal = parseInt(pesoAnimal);
+
+	 	 	if (isNaN(pesoAnimal) || pesoAnimal < 1 || pesoAnimal > 1000)
+	 	 	 {
+	 	 	 	alert("Ingrese solo un numero entre 1 y 1000");
+	 	 	 }
+	 	 }while(isNaN(pesoAnimal) || pesoAnimal < 1 || pesoAnimal > 1000);
+
+	 	do
+	 	 {
+	 	 	tempHabitat = prompt("Temperatura del habitat del animal (Solo un numero entre -30 y 30)");
+	 	 	tempHabitat = parseInt(tempHabitat);
+
+	 	 	if (isNaN(tempHabitat) || tempHabitat < (-30) || tempHabitat > 30)
+	 	 	 {
+	 	 	 	alert("Ingrese solo un numero entre -30 y 30");
+	 	 	 }
+	 	 }while(isNaN(tempHabitat) || tempHabitat < (-30) || tempHabitat > 30);
+
+		if (tempHabitat%2 == 0)
+		 {
+	 		cantTempPares ++;
+	 	 }
+
+		if (contador == 0)
+		 {
+	 		animalMasPesado = animal;
+	 		tempAnimalMasPesado = tempHabitat;
+	 		pesoAnimalMasPesado = pesoAnimal;
+		 }
+		else
+		 {
+		 	if (pesoAnimalMasPesado < pesoAnimal)
+		 	 {
+		 	 	animalMasPesado = animal;
+		 	 	tempAnimalMasPesado = tempHabitat;
+		 	 }
+		 }
+
+		if (contador == 0 && tempHabitat < 0)
+		 {
+	 		pesoAnimalMasPesadoBajoCero = pesoAnimal;
+	 		pesoAnimalMenosPesadoBajoCero = pesoAnimal;
+		 }
+
+		if (contador != 0 && tempHabitat < 0)
+		 {
+		 	if (pesoAnimalMasPesadoBajoCero < pesoAnimal)
+		 	 {
+		 	 	pesoAnimalMasPesadoBajoCero = pesoAnimal;
+		 	 }
+		 	if (pesoAnimalMenosPesadoBajoCero > pesoAnimal)
+		 	 {
+		 	 	pesoAnimalMenosPesadoBajoCero = pesoAnimal;
+		 	 }
+		 }
+
+		if (tempHabitat < 0)
+		 {
+		 	cantAnimalesBajoCero ++;
+		 }
+
+		cantAnimales ++;
+
+		sumaPesoAnimales += pesoAnimal;
+
+	 	contador = 1;
+
+	 	continuar = confirm("¿Desea continuar?");
+	 }
+
+	promedioPesoAnimales= sumaPesoAnimales/cantAnimales;
+
+	document.write("Cantidad de temperaturas pares = " + cantTempPares);
+	document.write("<br>El animal mas pesado es " + animalMasPesado + " y vive a " + tempAnimalMasPesado + "°");
+	document.write("<br>Hay " + cantAnimalesBajoCero + " animales que viven a temperaturas bajo cero");
+	document.write("<br>El promedio de peso de todos los animales es " + promedioPesoAnimales);
+	document.write("<br>El animal que vive a temperatura bajo cero mas pesado pesa " + pesoAnimalMasPesadoBajoCero + " kilos. Y el peso del animal que vive a temperatura bajo cero mas liviano es de " + pesoAnimalMenosPesadoBajoCero + " kilos.");
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -10,20 +10,22 @@ function mostrar()
 	var cantImpar = 0;
 	var cantCeros = 0;
 	var promedioPos;
+	var sumaPos = 0;
 	var sumaNeg = 0;
 	var contador = 0;
 	var continuar = true;
+	var cantPos = 0;
 
 	while (continuar == true)
 	{
 		do
 		{
 		letra = prompt("Ingrese una letra");
-			if (isNaN(letra) == false)
+			if (isNaN(letra) == false || letra.length != 1)
 			 {
-			 	alert("Ingrese solo letras");
+			 	alert("Ingrese solo una letra");
 			 }
-		}while (isNaN(letra) == false);
+		}while (isNaN(letra) == false || letra.length != 1);
 
 		do
 		{
@@ -37,18 +39,58 @@ function mostrar()
 		} while(isNaN(numero) || numero < (-100) || numero > 100);
 
 		if (numero%2 == 0)
-	 	{
+	 	 {
 	 		cantPar ++;
-	 	}
+	 	 }
 	 	else
-	 	{
+	 	 {
 	 		cantImpar++;
-	 	}
+	 	 }
+	 	if (numero == 0)
+	 	 {
+	 	 	cantCeros ++;
+	 	 }
+	 	if (numero > 0)
+	 	 {
+	 	 	sumaPos += numero;
+	 	 	cantPos ++;
+	 	 }
+	 	 else
+	 	  {
+	 	  	sumaNeg += numero;
+	 	  }
+	 	if (contador == 0)
+	 	 {
+	 	 	letraMinimo = letra;
+	 	 	letraMaximo = letra;
+	 	 	numMinimo = numero;
+	 	 	numMaximo = numero;
+	 	 	contador = 1;
+	 	 }
+	 	 else
+	 	 {
+	 	 	if (numero > numMaximo)
+	 	 	 {
+	 	 	 	numMaximo = numero;
+	 	 	 	letraMaximo = letra;
+	 	 	 }
+	 	 	if (numero < numMinimo)
+	 	 	 {
+	 	 	 	numMinimo = numero;
+	 	 	 	letraMinimo = letra;
+	 	 	 }
+	 	 }
 
 		continuar = confirm("¿Desea continuar?");
 	}
 
-	alert(numero + " " + letra);
+	promedioPos = sumaPos/cantPos;
 
+	alert(numero + " " + letra);
 	alert("Cantidad par = " + cantPar + ". Cantidad Impar = " + cantImpar);
+	alert("Cantidad de 0 = " + cantCeros);
+	alert("Promedio de los positivos = " + promedioPos);
+	alert("Suma de los negativos = " + sumaNeg);
+	alert("Letra y numero maximos = " + letraMaximo + " " + numMaximo + ". Letra y numero minimos = " + letraMinimo + " " + numMinimo);
+
 }
